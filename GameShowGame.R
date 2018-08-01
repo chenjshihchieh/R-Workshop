91144c0c0eb6e9402491e835ac15678b1b311130
Jack Chen <chen.j.shihchieh@gmail.com>
2018-07-31 20:01:54 -0700

    Second Revision

--- GameShowGame.R
+++ GameShowGame.R
@@ -1,55 +1,48 @@
 #The game show problem
 
 rm(list = ls())
-SelectedDoor <- A #A, B, or C
-Strategy <- "Stay" #Stay or Switch
 
+SelectedDoor <- 1 #1, 2, or 3 (without quotations)
+Strategy <- "Switch" #"Stay" or "Switch" (with quotations)
 
-win <- NULL
-denominator <- NULL
+if(!(exists("win") & exists("denominator"))){
+win <- 0
+denominator <- 0
+}
 
+A <- NULL
+B <- NULL
+C <- NULL
 
-  
+Items <- c("Car", "Empty", "Empty")
+Prize <- c(1, 2, 3)
+#Prize Dataframe
+prize.df <- data.frame(Prize, Items = sample(Items, 3))
 
-  Items <- c("Car", "Empty", "Empty")
-  Prize <- c("P1", "P2", "P3")
-  #Prize Dataframe
-  prize.df <- data.frame(Prize, Items = sample(Items, 3))
-  #Randomly assign prize to door
-  A <- sample(Prize, 1)
-  Prize <- Prize[Prize != A]
-  B <- sample(Prize, 1)
-  C <- Prize[Prize != B]
-  Door <- c(A, B, C)
-  #What is behind selected door
-  
+Door <- c(1, 2, 3)
+#What is behind selected door
 
-  #Playing the game
+#Playing the game
 if (Strategy == 'Stay'){
-  
-  if (prize.df[prize.df$Prize == SelectedDoor, "Items"] == 'Car'){
+  if (prize.df[SelectedDoor, "Items"] == 'Car'){
     win <-  win + 1
-    denominator <- denominator + 1
-    print(prize.df[prize.df$Prize == SelectedDoor, "Items"])
+    print(prize.df[SelectedDoor, "Items"])
     }else{
-    win <- win + 0
-    denominator <- denominator + 1
-    Prize.df[prize.df[prize.df$Prize == SelectedDoor, "Items"]
-             }
-} else if (prize.df[prize.df$Prize == SelectedDoor, "Items"] == 'Car'){
+    print(prize.df[SelectedDoor, "Items"])
+    }
+}else if (prize.df[SelectedDoor, "Items"] == 'Car'){
     Door <- sample(Door[-SelectedDoor], 1)
+    
     print(prize.df[Door, "Items"])
     win <- win + 0
-    denominator = denominator + 1
-  } else if (prize.df[prize.df$Prize == SelectedDoor, "Items"] == 'Empty'){
+}else if(prize.df[SelectedDoor, "Items"] == 'Empty'){
     Door <- Door[-SelectedDoor]
-    Door <- Door[prize.df[Prize.df$Items] == "Car", Prize]
+    Door <- Door[prize.df$Items[Door] == "Car"]
     print <- prize.df[Door, "Items"]
     win <- win + 1 
-    denominator = denominator + 1
-  }
+}
   
-
+denominator <- denominator + 1
 ProbabilityofWinning <- win/denominator
 ProbabilityofWinning
 
