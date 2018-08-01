a8896026ec7388dc27fcec5ac7dfd85d80e93549
Jack Chen <chen.j.shihchieh@gmail.com>
2018-07-31 20:05:42 -0700

    Simplified and made some changes

--- GameShowGame.R
+++ GameShowGame.R
@@ -5,40 +5,32 @@ rm(list = ls())
 SelectedDoor <- 1 #1, 2, or 3 (without quotations)
 Strategy <- "Switch" #"Stay" or "Switch" (with quotations)
 
+
 if(!(exists("win") & exists("denominator"))){
 win <- 0
 denominator <- 0
 }
 
-A <- NULL
-B <- NULL
-C <- NULL
-
-Items <- c("Car", "Empty", "Empty")
-Prize <- c(1, 2, 3)
-#Prize Dataframe
-prize.df <- data.frame(Prize, Items = sample(Items, 3))
-
+Items <- sample(c("Car", "Empty", "Empty"), 3)
 Door <- c(1, 2, 3)
 #What is behind selected door
 
 #Playing the game
 if (Strategy == 'Stay'){
-  if (prize.df[SelectedDoor, "Items"] == 'Car'){
+  if (Items[SelectedDoor] == 'Car'){
     win <-  win + 1
-    print(prize.df[SelectedDoor, "Items"])
+    print(Items[SelectedDoor])
     }else{
-    print(prize.df[SelectedDoor, "Items"])
+    print(Items[SelectedDoor])
     }
-}else if (prize.df[SelectedDoor, "Items"] == 'Car'){
+}else if (Items[SelectedDoor] == 'Car'){
     Door <- sample(Door[-SelectedDoor], 1)
-    
-    print(prize.df[Door, "Items"])
+    print(Items[Door])
     win <- win + 0
-}else if(prize.df[SelectedDoor, "Items"] == 'Empty'){
+}else if(Items[SelectedDoor] == 'Empty'){
     Door <- Door[-SelectedDoor]
-    Door <- Door[prize.df$Items[Door] == "Car"]
-    print <- prize.df[Door, "Items"]
+    Door <- Door[Items[Door] == "Car"]
+    print(Items[Door])
     win <- win + 1 
 }
   
