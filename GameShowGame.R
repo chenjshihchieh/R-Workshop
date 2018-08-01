538d0819b860deb5db19ef6795f1f1a81f269b1f
Jack Chen <chen.j.shihchieh@gmail.com>
2018-07-31 20:19:03 -0700

    Monty Hall game created as a function

--- GameShowGame.R
+++ GameShowGame.R
@@ -1,40 +1,22 @@
-#The game show problem
 
-rm(list = ls())
-
-SelectedDoor <- 1 #1, 2, or 3 (without quotations)
-Strategy <- "Switch" #"Stay" or "Switch" (with quotations)
-
-
-if(!(exists("win") & exists("denominator"))){
-win <- 0
-denominator <- 0
-}
+MontyHallGame <- function(SelectedDoor, Strategy){
+as.numeric(SelectedDoor)
+as.character(Strategy)
 
 Items <- sample(c("Car", "Empty", "Empty"), 3)
 Door <- c(1, 2, 3)
-#What is behind selected door
-
-#Playing the game
 if (Strategy == 'Stay'){
   if (Items[SelectedDoor] == 'Car'){
-    win <-  win + 1
-    print(Items[SelectedDoor])
+    Items[SelectedDoor]
     }else{
-    print(Items[SelectedDoor])
+    Items[SelectedDoor]
     }
 }else if (Items[SelectedDoor] == 'Car'){
     Door <- sample(Door[-SelectedDoor], 1)
-    print(Items[Door])
-    win <- win + 0
+    Items[Door]
 }else if(Items[SelectedDoor] == 'Empty'){
     Door <- Door[-SelectedDoor]
     Door <- Door[Items[Door] == "Car"]
-    print(Items[Door])
-    win <- win + 1 
+    Items[Door]
+    }
 }
-  
-denominator <- denominator + 1
-ProbabilityofWinning <- win/denominator
-ProbabilityofWinning
-
