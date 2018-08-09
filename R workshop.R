7797b4776176a5aae5051dd2b16683178e9f2e31
Jack Chen <chen.j.shihchieh@gmail.com>
2018-08-08 22:00:41 -0700

    R workshop Initial

---
+++ R workshop.R
@@ -0,0 +1,40 @@
+##Starting R
+#You can interact with R through the Console or in the script
+#Console automatically runs the code
+#Script requires you to press "Run" or ctrl + Enter
+1 + 1
+
+2 - 1
+
+#Multiply (*) and divide (/)
+2 * 3
+
+6 / 2
+
+#Squaring numbers (^)
+2 ^ 3
+
+#Square roots
+sqrt(25)
+
+#R follows BEDMAS/PEMDAS
+
+#IMPORTANT: R does not save your work unless prompted to
+#"<-" allows you to save values by assigning it a variable
+x <- 1 + 1
+
+#You can then call back those values by typing in the variable
+X
+
+#Those variables will take on the property of the assigned values
+X - 1
+Y <- X * 3
+Y / 2
+
+#You can assign multiple values to a variable
+x <- c(1, 2, 3, 4, 5, 6, 7, 8, 9)
+
+#Strings
+"characters"
+
+
