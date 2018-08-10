86fad5f7b03f6e82ccf91c089c24245af063716c
Jack Chen <chen.j.shihchieh@gmail.com>
2018-08-10 14:02:29 -0700

    Added instruction for three function and introduced stirngs and started writing about logicals

--- R workshop.R
+++ R workshop.R
@@ -21,25 +21,56 @@ sqrt(25)
 
 #IMPORTANT: R does not save your work unless prompted to
 #"<-" allows you to save values by assigning it a variable
-x <- 1 + 1
+a <- 2
 
 #You can then call back those values by typing in the variable
-X
+a
+
+#R also saves the result of a computation
+x <- 1 + 1
+
 
 #Those variables will take on the property of the assigned values
-X - 1
-Y <- X * 3
-Y / 2
+#In this case, the property of the values are numerical
+x - 1
+y <- x * 3
+y / 2
 
 #You can assign multiple values to a variable
+#However, that requires a function, c()
+#All function takes the form of FunctionName()
+x <- 1, 2, 3, 4, 5, 6, 7, 8, 9
 x <- c(1, 2, 3, 4, 5, 6, 7, 8, 9)
 
 #The variable can be any name as long as the name does not contain a space.
 #Common replacements are "_" and "."
 
-#In addition to numeric values, you can also store strings
-spider_size <- c("Small", "Small", "small", "small", "Big", "Big", "Big", "Big")
+sequence_of_number <- c(1, 2, 3, 4, 5, 6, 7, 8, 9)
 
-#
+#Is there an easier way to generate a list of numbers? Google!
+#Generate a list of numbers from 1 to 50
 
 
+###########################################################################
+###########################################################################
+
+#In addition to numeric values, you can also store strings (aka characters)
+example <- c("Apple", "Orange", " ", "1", "2")
+condition <- c("Control", "Control", "Control", "Experimental", "Experimental", "Experimental")
+
+#Using the repeat function, rep()
+#using help() or ? to find out how to use the function
+help(rep)
+?rep
+
+#Use function to generate the condition variable
+
+
+###########################################################################
+###########################################################################
+#You can also reference any object in variables using square brackets, []
+sequence_of_number[5]
+condition[3]
+condition[4]
+
+#You can also have Logical values in the forms of TRUE and FALSE
