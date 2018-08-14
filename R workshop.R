40a81f04a319c85fdaa7374b3d61d04303df1946
Jack Chen <chen.j.shihchieh@gmail.com>
2018-08-14 00:44:25 -0700

    Made major changes with order and presentation

--- R workshop.R
+++ R workshop.R
@@ -47,8 +47,8 @@ y * result2
 ###########################################################################
 #All function takes the form of FunctionName()
 #c() allows us to concatenate a list of objects into one object
-x <- 1, 2, 3, 4, 5, 6, 7, 8, 9
-x <- c(1, 2, 3, 4, 5, 6, 7, 8, 9)
+number <- 1, 2, 3, 4, 5, 6, 7, 8, 9
+number <- c(1, 2, 3, 4, 5, 6, 7, 8, 9)
 
 #The variable can be any name as long as the name does not contain a space.
 #Common replacements are "_", "." and capitilizing the next word
@@ -60,70 +60,86 @@ sequenceNumber <- c(1, 2, 3, 4, 5, 6, 7, 8, 9)
 #Is there an easier way to generate a list of numbers? Google!
 #Generate a list of numbers from 1 to 50 and assign it to a variable
 
+
 ###########################################################################
 ###########################################################################
 #Each object in your list is given a number
-#You can reference any object in variables with square brackets, [], by referencing those numbers
-sequenceNumber[5]
-sequenceNumber[2]
-sequenceNumber[4]
+#You can extract any object in variables with square brackets, [], by referencing those numbers
+#This is indexing
+number[5]
+number[2]
+number[4]
 
 #What happens in these cases?
-sequenceNumber[1 + 1]
-sequenceNumber[8/2]
-sequenceNumber[2, 4]
-sequenceNumber[c(2, 4)]
+number[1 + 1]
+number[8/2]
+number[2, 4]
+number[c(2, 4)]
 
 #Try to reference all odd/even number objects
 
+
+###########################################################################
+###########################################################################
+#You can also have Logical values in the forms of TRUE and FALSE
+#You can use various logical operator 
+# (>, <, >=, <=, ==, =/=)
+5 > 3
+5 < 3
+
+#the operators also apply to variables
+x <- 2
+x == 2
+x > 3
+example <- c(1:4)
+example < 3
+example == 3
+
+#you can use logicals to index a variable
+example[c(TRUE, TRUE, FALSE, FALSE)]
+example[c(TRUE, FALSE)]
+example[example < 3]
+example[example == 2]
 
 
 
 ###########################################################################
 ###########################################################################
 #In addition to numeric values, there are also strings (aka characters)
-#example <- c("Apple", "Orange", " ", "1", "2") 
-condition <- c("Control", "Control", "Control", "Experimental", "Experimental", "Experimental")
+fruits <- c("Apple", "Apple", "Orange", "Orange", "Banana", "Pineapple", "Watermelon", "Banana", "Watermelon", "Kiwi")
 
+#Generate a list of conditions containing 50 "Control" and 50 "Experimental" and assign it a variable
 #Making this process easier Using the repeat function, rep()
 #using help() or ? to find out how to use the function
 help(rep)
 ?rep
 
-#Generate a list of conditions containing 25 "Control" and 25 "Experimental" and assign it a variable
-
-#
-
 
 ###########################################################################
 ###########################################################################
-#You can also have Logical values in the forms of TRUE and FALSE
-#Logical value can also be stored like others
-logic <- TRUE
-logic2 <- FALSE
-logicVector <- c(TRUE, FALSE, TRUE)
+#Generating a data with normal distribution
+#Create some data using rnorm()
+data1 <- rnorm(n= 50, mean = 10, sd = 2)
+#We can test to see if this is correct
+mean(data1)
+sd(data1)
 
-#You can use various logical operator 
+data2 <- rnorm(n=50, mean = 20, sd = 3)
+#We can test to see if this is correct
+mean(data2)
+sd(data2)
+
+#You can graph out each variable to see if they are truely normal
+hist(data1)
+hist(data2)
+
+#We will come back to these data later
 
 
 ###########################################################################
 ###########################################################################
 #A variable with a list of objects is only one dimensionl
 #Use data frames to create a 2 dimensional chart
-#First, create some data using rnorm()
-Control <- rnorm(n= 50, mean = 10, sd = 2)
-#We can test to see if this is correct
-mean(Control)
-sd(Control)
-
-Experimental <- rnorm(n=50, mean = 20, sd = 3)
-#We can test to see if this is correct
-mean(Experimental)
-sd(Experimental)
-
-#You can graph out each variable to see if they are truely normal
-hist(Control)
-hist(Experimental)
 
 #combine the 2  one-dimensional data
 #Don't forget to save it to a variable because R won't do it automatically
